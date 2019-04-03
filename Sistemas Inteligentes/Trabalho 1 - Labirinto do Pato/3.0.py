6dc931a52a6bc218843c48a98530e7b614855d2e
Leonardo Reis <leonardomfreis@gmail.com>
2019-04-03 19:30:31 -0300

    a

--- Sistemas Inteligentes/Trabalho 1 - Labirinto do Pato/3.0.py
+++ Sistemas Inteligentes/Trabalho 1 - Labirinto do Pato/3.0.py
@@ -1,5 +1,80 @@
 from collections import deque, defaultdict
-from itertools import cycle
+import math
+
+class Agent:
+
+    def __init__(self, pos, blacklisted_pos, objective_pos, w, h):
+        self.pos = pos  # pos[x][y]
+        self.blacklisted_pos = blacklisted_pos
+        self.w = w
+        self.h = h
+        self.objective_pos = objective_pos
+        self.objective_pos.append(6)
+
+    def checkpos(self, pos):
+        if pos in self.blacklisted_pos:
+            return 0
+        if pos[0] < 0 or pos[0] >= self.h:
+            return 0
+        if pos[1] < 0 or pos[1] >= self.w:
+            return 0
+        else:
+            return 1
+
+    direct = [6,3,2,1,4,7,8,9]
+
+    def ir(self,x):
+        old_pos = self.pos.copy()
+        new_pos = self.pos.copy()
+        if x == "4":
+            new_pos[2] = self.direct[(self.direct.index(new_pos[2])-1)]
+        elif x == '6':
+            new_pos[2] = self.direct[(self.direct.index(new_pos[2])+1) % len(self.direct)]
+        elif x == '8':
+            new_pos = self.irpara(str(new_pos[2]))
+        else:
+            print("Wrong Input Try Again!")
+            return old_pos
+        if self.checkpos(new_pos) == 1:
+            return new_pos
+        else:
+            return old_pos
+
+    def irpara(self, x):
+        old_pos = self.pos.copy()
+        new_pos = self.pos.copy()
+        if x == 'N' or x == '8':
+            new_pos[0] += 1
+        elif x == 'NE' or x == '9':
+            new_pos[1] += 1
+            new_pos[0] += 1
+        elif x == 'L' or x == '6':
+            new_pos[1] += 1
+
+        elif x == 'SE' or x == '3':
+            new_pos[1] += 1
+            new_pos[0] -= 1
+
+        elif x == 'S' or x == '2':
+            new_pos[0] -= 1
+
+        elif x == 'SO' or x == '1':
+            new_pos[1] -= 1
+            new_pos[0] -= 1
+
+        elif x == 'O' or x == '4':
+            new_pos[1] -= 1
+
+        elif x == 'NO' or x == '7':
+            new_pos[1] -= 1
+            new_pos[0] += 1
+        else:
+            print("Wrong Input Try Again!")
+            return old_pos
+        if self.checkpos(new_pos) == 1:
+            return new_pos
+        else:
+            return old_pos
 
 
 class Grid:
@@ -43,84 +118,103 @@ class Grid:
             return 1
 
 
-class Agent:
+def bfs():
+    best_route = []
+    objective = agent.objective_pos
+    objective = str(objective)
+    frontier = deque()
+    frontier.append(agent.pos)
+    while len(frontier) > 0:
+        agent.pos = frontier[0]
+        for dire in directions:
+            new_dir = agent.ir(dire)
+            if new_dir != agent.pos and new_dir not in visited and new_dir not in agent.blacklisted_pos:
+                frontier.append(new_dir)
+                visited.append(new_dir)
+                foundby[str(new_dir)] = str(agent.pos)
+        if agent.pos == agent.objective_pos:
+            break
 
-    def __init__(self, pos, blacklisted_pos, objective_pos, w, h):
-        self.pos = pos  # pos[x][y]
-        self.blacklisted_pos = blacklisted_pos
-        self.w = w
-        self.h = h
-        self.objective_pos = objective_pos
-        self.objective_pos.append(6)
+        print('Current:')
+        print(agent.pos)
+        print(frontier)
+        visited.append(agent.pos)
+        frontier.popleft()
+    while objective != str(gridc.init_pos):
+        objective = foundby[objective]
+        best_route.append(objective)
+    best_route.insert(0,agent.objective_pos)
+    print("best path")
+    print(best_route)
 
-    def checkpos(self, pos):
-        if pos in self.blacklisted_pos:
-            return 0
-        if pos[0] < 0 or pos[0] >= self.h:
-            return 0
-        if pos[1] < 0 or pos[1] >= self.w:
-            return 0
-        else:
-            return 1
-
-    direct = [6,3,2,1,4,7,8,9]
-
-    def ir(self,x):
-        old_pos = self.pos.copy()
-        new_pos = self.pos.copy()
-        if x == "4":
-            new_pos[2] = self.direct[(self.direct.index(new_pos[2])-1)]
-        elif x == '6':
-            new_pos[2] = self.direct[(self.direct.index(new_pos[2])+1) % len(self.direct)]
-        elif x == '8':
-            new_pos = self.irpara(str(new_pos[2]))
-        else:
-            print("Wrong Input Try Again!")
-            return old_pos
-        if self.checkpos(new_pos) == 1:
-            return new_pos
-        else:
-            return old_pos
+def takeEle(ele):
+    return ele[3]
 
 
+def astar():
+
+    yo, xo, do = agent.objective_pos
+    agent.pos.append(0)
+    ay,ax,ad,av = agent.pos
+    frontier = deque()
+    print(frontier)
+    frontier.append(agent.pos)
+
+    for y, row in enumerate(grid):
+        for x, line in enumerate(row):
+            values_grid[y][x] = math.sqrt((((y - yo) ** 2) + ((x - xo) ** 2)))
+
+    for a, b in agent.blacklisted_pos:
+        values_grid[a][b] = 99
 
 
 
-    def irpara(self, x):
-        old_pos = self.pos.copy()
-        new_pos = self.pos.copy()
-        if x == 'N' or x == '8':
-            new_pos[0] += 1
-        elif x == 'NE' or x == '9':
-            new_pos[1] += 1
-            new_pos[0] += 1
-        elif x == 'L' or x == '6':
-            new_pos[1] += 1
+    #acha os vizinhos
 
-        elif x == 'SE' or x == '3':
-            new_pos[1] += 1
-            new_pos[0] -= 1
 
-        elif x == 'S' or x == '2':
-            new_pos[0] -= 1
 
-        elif x == 'SO' or x == '1':
-            new_pos[1] -= 1
-            new_pos[0] -= 1
+    while len(frontier) > 0:
+        agent.pos = frontier[0]
+        for dire in directions:
+            new_dir = agent.ir(dire)
+            if new_dir != agent.pos and new_dir not in visited and new_dir not in agent.blacklisted_pos:
+                new_dir[3] = values_grid[new_dir[0]][new_dir[1]]
+                frontier.append(new_dir)
+                visited.append(new_dir)
+                foundby[str(new_dir)] = str(agent.pos)
 
-        elif x == 'O' or x == '4':
-            new_pos[1] -= 1
+        print('Current:')
+        print(agent.pos)
+        print(frontier)
+        visited.append(agent.pos)
+        frontier.popleft()
+        frontier = deque(sorted(frontier, key=takeEle))
 
-        elif x == 'NO' or x == '7':
-            new_pos[1] -= 1
-            new_pos[0] += 1
-        else:
-            print("Wrong Input Try Again!")
-            return old_pos
-        if self.checkpos(new_pos) == 1:
-            return new_pos
-        else:
-            return old_pos
+
+
+
+
+    #para seres humanos verem
+    visualize = [[2, 1, 1, 1, 2, 3, 4, 5, 0, 7],
+                 [2, 0, 0, 1, 2, 3, 0, 5, 0, 7],
+                 [2, 1, 1, 1, 2, 3, 4, 5, 0, 7],
+                 [3, 0, 2, 2, 0, 4, 0, 5, 0, 7],
+                 [4, 3, 3, 3, 4, 4, 5, 6, 7, 8],
+                 [0, 0, 0, 0, 0, 0, 6, 6, 0, 8],
+                 [5, 5, 5, 5, 5, 6, 6, 7, 8, 9],
+                 [6, 6, 0, 6, 0, 7, 0, 8, 0, 9],
+                 [7, 7, 7, 7, 7, 8, 8, 9, 9, 10],
+                 [8, 8, 8, 8, 8, 9, 9, 9, 10, 11]]
+
+    for x in visualize:
+        print(x)
+
+
+    #calcula a distancia de cada celula
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -137,56 +231,16 @@ if __name__ == '__main__':
     visited = []
     foundby = {}
 
+    values_grid = [[0] * agent.w for i in range(agent.h)]
+
+
     agent.pos.append(6)
 
 
-    def bfs():
-        best_route = []
-        objective = agent.objective_pos
-        objective = str(objective)
-        frontier = deque()
-        frontier.append(agent.pos)
-        while len(frontier) > 0:
-            agent.pos = frontier[0]
-            for dire in directions:
-                new_dir = agent.ir(dire)
-                if new_dir != agent.pos and new_dir not in visited and new_dir not in agent.blacklisted_pos:
-                    frontier.append(new_dir)
-                    visited.append(new_dir)
-                    foundby[str(new_dir)] = str(agent.pos)
-            if agent.pos == agent.objective_pos:
-                break
-
-            print('Current:')
-            print(agent.pos)
-            print(frontier)
-            visited.append(agent.pos)
-            frontier.popleft()
-        while objective != str(gridc.init_pos):
-            objective = foundby[objective]
-            best_route.append(objective)
-        best_route.insert(0,agent.objective_pos)
-        print("best path")
-        print(best_route)
-
-    def astar():
-        print('Vamos Tentar')
+    astar()
 
 
-    bfs()
 
-'''    while 1:
-        ipt = input('Direção')
-        old_pos = agent.pos
-        new_pos = agent.ir(ipt)
-        agent.pos = new_pos
-        grid[old_pos[0]][old_pos[1]] = 0
-        grid[new_pos[0]][new_pos[1]] = 3
-        for line in reversed(grid):
-            print(line)'''
-'''    if agent.pos == agent.objective_pos:
-        print("you win!")
-        break
-'''
 
+    #bfs()
 
